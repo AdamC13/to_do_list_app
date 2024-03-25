1b3ffbb5f3584043358a82958af6c34b7e2a918d
AdamC13 <acif333@gmail.com>
2024-03-24 23:41:16 -0400

    Added whitespace error for add_task function

--- to_do_list_app.py
+++ to_do_list_app.py
@@ -8,6 +8,7 @@ def cli():
     3. Mark a task as complete
     4. Delete a task
     5. Quit
+                       
     """)
     try:
         if int(user_input) == 1:
@@ -33,6 +34,12 @@ def add_task():
     task = input("\nWhat task would you like to add? (back) ")
     if task.lower() == "back":
         cli()
+    elif task.isspace() == True:
+        print("\n\n\nInvalid input ༼ つ ◕_◕ ༽つ")
+        add_task()
+    elif task == '':
+        print("\n\n\nInvalid input ༼ つ ◕_◕ ༽つ")
+        add_task()
     else:
         tasks.append([task, False])
         add_task()
@@ -40,22 +47,22 @@ def add_task():
     
 
 def view_task():
-    print("\nHere are your tasks so far")
+    print("\nHere are your tasks so far\n")
     for task in tasks:
         if task[1] == True:
-            print(f"{task[0]} --> Complete! 😎")
+            print(f"{task[0]} --> Complete! ✅ 😎")
         elif task[1] == False:
-            print(f"{task[0]} --> Incomplete 🙄")
+            print(f"{task[0]} --> Incomplete ❌ 🙄")
     input("\nPress enter to go back")
     cli()
 
 def mark_complete():
-    print("\nWhich task did you complete?")
+    print("\nWhich task did you complete?\n")
     for i in range(len(tasks)):
         if tasks[i][1] == True:
-            print(f"{i+1}: {tasks[i][0]} --> Already complete! 😎")
+            print(f"{i+1}: {tasks[i][0]} --> Already complete! ✅ 😎")
         elif tasks[i][1] == False:
-            print(f"{i+1}: {tasks[i][0]} --> Incomplete 🙄")
+            print(f"{i+1}: {tasks[i][0]} --> Incomplete ❌ 🙄")
     user_input = input()
     if user_input == "quit":
         cli()
@@ -69,16 +76,19 @@ def mark_complete():
 
 
 def delete_task():
-    print("\nWhich task did you want to delete?")
+    print("\nWhich task did you want to delete?\n")
     for i in range(len(tasks)):
         print(f"{i+1}: {tasks[i][0]}")
     user_input = input()
     if user_input == "quit":
         cli()
     else:
-        tasks.pop(int(user_input)-1)
-        delete_task()
-
+        try:
+            tasks.pop(int(user_input)-1)
+            delete_task()
+        except(ValueError, IndexError, OverflowError):
+            print("\n\n\nInvalid input ༼ つ ◕_◕ ༽つ")
+            delete_task()
 
 
 tasks = [["This is a task", False], ["Another task", True]]
